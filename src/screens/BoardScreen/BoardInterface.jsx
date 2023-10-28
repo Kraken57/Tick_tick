4ac4ef13b2851daf635189b58ed829be53b86027
Kraken57 <kasaad1609@gmail.com>
2023-10-28 21:50:25 +0530

    board modal

--- src/screens/BoardScreen/BoardInterface.jsx
+++ src/screens/BoardScreen/BoardInterface.jsx
@@ -1,10 +1,14 @@
 import { Grid } from '@mui/material'
 import BoardTab from './BoardTab';
+import AddTaskModal from './AddTaskModal';
+
+const tabs
 
 
 const BoardInterface = () => {
     return(
         <>
+        {/*<AddTaskModal /> */ }
         <Grid container px={4} mt={2} spacing={2}>
             <BoardTab />
             
